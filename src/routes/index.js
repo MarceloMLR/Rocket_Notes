38bcbf1d15f688555f553706629118462d12f244
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2023-11-21 23:05:16 -0300

    Criando NotesController e cadastrando nota

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const router = require("express");
 
 const usersRoutes = require("./user.routes");
+const notesRoutes = require("./notes.routes");
 
 const routes = router();
 
 routes.use('/users', usersRoutes)
+routes.use('/notes', notesRoutes)
 
 module.exports = routes
