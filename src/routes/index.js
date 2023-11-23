28b8af66229241e280adb08bb9d5ae8d2f555ccc
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2023-11-23 00:28:33 -0300

    Criando Controller de Tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const router = require("express");
 
 const usersRoutes = require("./user.routes");
 const notesRoutes = require("./notes.routes");
+const tagsRoutes = require("./tags.routes");
 
 const routes = router();
 
 routes.use('/users', usersRoutes)
 routes.use('/notes', notesRoutes)
+routes.use('/tags', tagsRoutes)
 
 module.exports = routes
