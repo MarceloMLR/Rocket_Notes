12e31e1804eb74922c5cdce1fe0c7e3ed400eb71
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2024-01-15 00:21:24 -0300

    Configurando rota de upload de imagem

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,19 @@
 const { Router } = require("express");
-
-const usersRoutes = Router();
-
+const multer = require("multer");
+const uploadConfigs = require("../configs/upload")
 const UsersController = require("../controllers/UsersController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
+const usersRoutes = Router(); 
+const upload = multer(uploadConfigs.MULTER)
 
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
+  console.log(req.file.filename);
+  res.json();
+})
 
 module.exports = usersRoutes;
