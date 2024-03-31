e081ca8d1cda08ea2372158e569ffd4077e07c31
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2024-03-30 22:23:33 -0300

    corrigido import do DiskStorage

--- src/controllers/UsersAvatarController.js
+++ src/controllers/UsersAvatarController.js
@@ -1,4 +1,4 @@
-const DiskStorage = require("../providers/diskStorage");
+const DiskStorage = require("../providers/DiskStorage");
 const AppError = require("../utils/AppError");
 knex = require("../database/knex");
 
